6f5a7a455bc109547e4655c6c472bf84ae8177a0
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2025-07-23 22:00:37 +0300

    feat: pagination pipe and interceptor

---
+++ src/pagination/interfaces/pagination.interace.ts
@@ -0,0 +1,35 @@
+export type SortDirection = 'ASC' | 'DESC';
+
+export interface PaginationQueries {
+  page?: string;
+  limit?: string;
+  sort?: string;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  meta: PaginationMeta;
+}
+
+export interface PaginationMeta {
+  currentPage: number;
+  totalPages: number;
+  totalItems: number;
+  itemsPerPage: number;
+  itemsInPage?: number;
+  sortOptions?: Record<string, SortDirection>;
+  hasNextPage?: boolean;
+  hasPrevPage?: boolean;
+}
+
+export interface PaginationOptions {
+  page: number;
+  limit: number;
+  skip?: number;
+  sort?: Record<string, SortDirection>;
+}
+
+export interface ItemsWithTotal<T> {
+  items: T[];
+  total: number;
+}
